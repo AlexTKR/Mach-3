82dd405796d6d04ddf3ea17afca2cc510376b86e
Aleksei <alex.toktoraliev@gmail.com>
2022-09-19 17:39:49 +0300

    Game assets were added

---
+++ Match-3/Assets/Installers/MainSceneInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Installers
+{
+    public class UntitledInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
